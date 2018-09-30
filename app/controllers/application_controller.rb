2e7f5b3393283a49b1f5d28e30ca337b657a8eb7
Mary Jack <maryjackinthebox@gmail.com>
2018-09-30 23:23:52 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+  if !!@user = User.find_by(username: params["username"])
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
